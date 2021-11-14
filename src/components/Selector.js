82b2fe2edae45e2668a63e1bb8408e6616769487
Meg <miwa.egner@gmail.com>
2021-11-15 00:02:39 +0100

    europe added

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -1,19 +1,19 @@
 // Selector.js
-import countriesJson from '../countries_reduced.json';
+import countriesJson from '../countries_europe.json';
 
 const Selector = () => {
     //    console.log(countriesJson)
-    countriesJson.map((country) =>
-        console.log(country)
-    )
     return (
         <div>
             <p>
                 selector
             </p>
             <select>
-                <option>United States</option>
-                <option>China</option>
+                {
+                    countriesJson.map((country) =>
+                        <option> {country.Country} </option>
+                    )
+                }
             </select>
 
 
