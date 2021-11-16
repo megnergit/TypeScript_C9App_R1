c5504740351a40964f8743022c2d3bc41e7340a5
Meg <miwa.egner@gmail.com>
2021-11-16 04:37:46 +0100

    Selector.js updated

--- src/components/Selector.js
+++ src/components/Selector.js
@@ -6,13 +6,14 @@ const Selector = () => {
     //    console.log(countriesJson)
     const [country, setCountry] = useState('');
     const getCountryData = () => {
-        fetch("https://api.covid19api.com/country/japan")
+        fetch(`https://api.covid19api.com/country/${country}`)
             .then(res => res.json())
             .then(data => console.log(data))
     }
     return (
         <div>
             <select onChange={(e) => setCountry(e.target.value)}>
+                <option>Select A Country</option>
                 {
                     countriesJson.map((country, index) =>
                         <option key={index} value={country.Slug}>
@@ -20,9 +21,9 @@ const Selector = () => {
                         </option>
                     )
                 }
-            </select> {country}
-            <button onClick={getCountryData}>Get Data
-
+            </select>
+            <button onClick={getCountryData}>
+                Get Data
             </button>
         </div >
     );
